5d5510a71d4d990015c4282065beae962d17305b
Liliane Lima Castro <103222391+LilasLi@users.noreply.github.com>
2025-04-01 18:11:19 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,30 @@
-#define F_CPU   16000000
-#include <util/delay.h>
+#define F_CPU 16000000L
 #include <avr/io.h>
-
+#include <util/delay.h>
 int main(void) 
-{
-	// Configurações das entradas e saídas
-	// Entradas
-	DDRB &= ~((1<<2)|(1<<1));
-	PORTB |= ((1<<2)|(1<<1));	// Pull-up 
+{   
+    // config entradas
+    DDRB&= ~(1 << 1) | (1 << 2); // PB1 PB2 e como entradas
+    PORTB |= (1 << 1) | (1 << 2); // ativa pull up PB1 PB2
 
-	// Saídas
-	DDRB |= ((1<<3)|(1<<4)|(1<<5));
-	PORTB &= ~(1<<3); // Zero 
-	PORTB &= ~(1<<4); // Zero 
-	PORTB &= ~(1<<5); // Zero 
-
-	while( 1 )
-	{
-		if( !(PINB & (1<<2)) ) // S1
-		{
-			PORTB |= (1<<5);	// K1
-			PORTB |= (1<<4);	// K2
-			_delay_ms(5000);
-			PORTB &= ~(1<<4);	// K2
-			PORTB |= (1<<3);	// K3
-		}
-		else if( !(PINB & (1<<1)) ) // S0
-		{
-			PORTB &= ~(1<<3); // Zero 
-			PORTB &= ~(1<<4); // Zero 
-			PORTB &= ~(1<<5); // Zero 
-		}
-	}
+    // config saídas
+    DDRB &= |= ((1 << 3) | (1 << 4) | (1 << 5)); // PB3 PB4 PB5 como saídas
+    PORTB &= ~((1 << 3) | (1 << 4) | (1 << 5)); // desativa pull up PB3 PB4 PB5
+    while (1)
+    {
+        if( !(PINB & (1 << 1))) // se S1 (PB1) pressionado
+        {
+            PORTB |= (1 << 5); // ativa K1 (PB5)
+            PORTB |= (1 << 4); // ativa K2 (PB4)
+            _delay_ms(5000); // espera 5 segundo
+            PORTB &= ~(1 << 4); // desativa K2 (PB4)
+            PORTB &= ~(1 << 3); // desativa K3 (PB3)
+        }
+        else( !(PINB & (1 << 2))) // se S0 (PB2) pressionado
+        {
+            PORTB &= ~(1 << 3); // desativa K3 (PB3)
+            PORTB &= ~(1 << 4); // desativa K2 (PB4)
+            PORTB &= ~(1 << 5); // desativa K1 (PB5)
+        }
+    }
 }
