4b4967c689557c9b22ea1b2787d17c74724463e4
LilasLi <lilianeslima@outlook.com>
2025-04-03 10:58:56 -0300

    teste

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 #include <util/delay.h>
 int main(void) 
 {   
-    DDRB = 0b00110000;
-    PORTB = 0;
+    // config entradas
+    DDRB &= ~(1 << 1) | (1 << 2); // PB1 PB2 e como entradas
+    PORTB |= (1 << 1) | (1 << 2); // ativa pull up PB1 PB2
+
+    // config saídas
+    DDRB |= ((1 << 3) | (1 << 4) | (1 << 5)); // PB3 PB4 PB5 como saídas
+    PORTB &= ~((1 << 3) | (1 << 4) | (1 << 5)); // desativa pull up PB3 PB4 PB5
     while (1)
     {
-        if( PINB & (1<<PINB0)) // Leitura do fim de curso
+        if( !(PINB & (1 << 2))) // se S1 (PB1) pressionado
         {
-            PORTB |= (1<<PORTB4);
-            PORTB &= ~(1<<PORTB5);
-            _delay_ms(500);
-            PORTB ^= (1<<PORTB4 | 1<<PORTB5);
-            _delay_ms(500);
+            PORTB |= (1 << 5); // ativa K1 (PB5)
+            PORTB |= (1 << 4); // ativa K2 (PB4)
+            _delay_ms(5000); // espera 5 segundo
+            PORTB &= ~(1 << 4); // desativa K2 (PB4)
+            PORTB |= (1 << 3); // ativa K3 (PB3)
         }
-        else
+        else if( !(PINB & (1 << 1))) // se S0 (PB2) pressionado
         {
-            PORTB &= ~(1<<PORTB4);
-            PORTB &= ~(1<<PORTB5);
+            PORTB &= ~(1 << 3); // desativa K3 (PB3)
+            PORTB &= ~(1 << 4); // desativa K2 (PB4)
+            PORTB &= ~(1 << 5); // desativa K1 (PB5)
         }
     }
 }
